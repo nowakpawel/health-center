09024f78a4e0531fd340f43b14ab31eb26b4c166
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2021-03-05 17:08:08 +0100

    Add Spring initial files

---
+++ src/main/java/pl/nowak/pawel/healthcenter/HealthCenterApplication.java
@@ -0,0 +1,13 @@
+package pl.nowak.pawel.healthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCenterApplication.class, args);
+	}
+
+}
